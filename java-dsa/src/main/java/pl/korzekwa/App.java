dc8c5f5dfd18cb0007d8e436119cc782184d9ef9
Kacper Korzekwa <kackor11@gmail.com>
2026-04-17 18:04:07 +0200

    Project setup

---
+++ java-dsa/src/main/java/pl/korzekwa/App.java
@@ -0,0 +1,13 @@
+package pl.korzekwa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
